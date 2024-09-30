69bc585c25490b558d41a7cb4dc6616d6149851b
Maarten Vandeperre @ Red Hat <mvandepe@redhat.com>
2024-09-30 16:42:00 +0200

    wrap up testing

--- camel-integrations/build_and_deploy_fat_jar.sh
+++ camel-integrations/build_and_deploy_fat_jar.sh
@@ -3,9 +3,9 @@
 
 VERSION=0.0.5 # adjust src/main/manifests/deployment-fatjar.yaml as well
 
-docker build --platform linux/amd64 -t quay.io/rh_ee_mvandepe/cdc-based-integration-example-camel:$VERSION-fatjar \
+podman build --platform linux/amd64 -t quay.io/rh_ee_mvandepe/cdc-based-integration-example-camel:$VERSION-fatjar \
         -f /src/main/container/ContainerImageDefinition \
         .
-docker push quay.io/rh_ee_mvandepe/cdc-based-integration-example-camel:$VERSION-fatjar
+podman push quay.io/rh_ee_mvandepe/cdc-based-integration-example-camel:$VERSION-fatjar
 
 oc apply -f src/main/manifests/deployment-fatjar.yaml
